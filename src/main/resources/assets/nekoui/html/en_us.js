9cf6ab40a13722445011a4f4457de9f3b6be113c
An-Mao <lxxl@live.cn>
2026-07-15 17:16:51 +0800

    26071500

--- src/main/resources/assets/nekoui/html/en_us.js
+++
@@ -1,5 +0,0 @@
-{
-    "default_title": "General Settings",
-    "label_key": "Config Key",
-    "error_message": "Connection error, could not load data."
-}
--- src/main/resources/assets/nekoui/html/zh_cn.js
+++
@@ -1,5 +0,0 @@
-{
-    "default_title": "通用设置",
-    "label_key": "配置Key",
-    "error_message": "连接错误，无法加载数据。"
-}
